832d3a02b5874dad3d208fe1f581aa6f6e5e6aa3
Juri Boiko <jboiko@williamhill.co.uk>
2017-01-13 17:12:16 +0100

    update

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -11,9 +11,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Stream;
 
-import static spark.Spark.get;
-import static spark.Spark.post;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 /**
  * Created by Juri on 11/01/2017.
@@ -22,6 +20,19 @@ public class Main {
     public static void main(String[] args) {
         staticFileLocation("/public");
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
+
+        before((req, res) -> {
+            if (req.cookie("username") == null)  {
+               req.attribute("username", req.cookie("username"));
+            }
+        });
+
+        before("/ideas", (req, res) -> {
+            if (req.cookie("username") == null)  {
+            res.redirect("/");
+                halt();
+            }
+        });
 
         get("/", (req, res) -> {
             Map<String, String> model = new HashMap<>();
@@ -45,8 +56,21 @@ public class Main {
 
         post("/ideas", (req, res) -> {
             String title = req.queryParams("title");
-            CourseIdea courseIdea = new CourseIdea(title, req.cookie("username"));
+            CourseIdea courseIdea = new CourseIdea(title, req.attribute("username"));
             dao.add(courseIdea);
+            res.redirect("/ideas");
+            return null;
+        });
+
+        get ("/ideas/:slug", (req, res) -> {
+           Map<String, Object> model = new HashMap<>();
+            model.put("idea", dao.findBySlug(req.params("slug")));
+            return new ModelAndView(model, "idea.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/ideas/:slug/vote", (req, res) -> {
+           CourseIdea idea = dao.findBySlug(req.params("slug"));
+            idea.addVoter(req.attribute("username"));
             res.redirect("/ideas");
             return null;
         });
---
+++ src/main/java/com/teamtreehouse/courses/model/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.teamtreehouse.courses.model;
+
+/**
+ * Created by Juri on 13/01/2017.
+ */
+public class NotFoundException extends RuntimeException{
+}
